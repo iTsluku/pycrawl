4c72d80949bf7bbc066aab9cae4b2a97a5ce1bf1
einwille <einwille@fim.uni-passau.de>
2023-05-25 18:03:55 +0200

    Add ReturnCode and logging

--- pypong/cli.py
+++ pypong/cli.py
@@ -1,2 +1,28 @@
-def main(argv) -> None:
-    pass
+import enum
+import logging
+
+
+@enum.unique
+class ReturnCode(enum.IntEnum):
+    """Return codes for pypong."""
+
+    OK = 0
+    """Successful execution with expected ending."""
+
+    SETUP_FAILED = 1
+    """Execution failed in the setup phase."""
+
+
+logging.basicConfig(
+    format="%(asctime)s [%(levelname)s] (%(name)s:%(funcName)s:%(lineno)d): %(message)s",
+    level=logging.INFO,
+)
+logger = logging.getLogger(__name__)
+
+
+def main(argv) -> int:
+    # TODO setup
+    logger.info("Setup env.")
+    # TODO parse argv
+    logger.info("Parse input arguments.")
+    return ReturnCode.OK

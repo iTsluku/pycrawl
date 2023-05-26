2904b669ed0a2968f5afbdff02cf957a81c657c0
einwille <einwille@fim.uni-passau.de>
2023-05-26 18:21:03 +0200

    Refactor setup process, Extend parser, Init solver skeleton

--- pycrawl/cli.py
+++ pycrawl/cli.py
@@ -15,7 +15,10 @@ class ReturnCode(enum.IntEnum):
     OK = 0
     """Successful execution with expected ending."""
 
-    RETRIEVAL_FAILED = 1
+    SETUP_FAILED = 1
+    """No relevant documents could be found."""
+
+    RETRIEVAL_FAILED = 2
     """No relevant documents could be found."""
 
 
@@ -31,12 +34,16 @@ def main(argv: Optional[List[str]]) -> int:
     argv: List[str] = strip_argv(argv)
     parser: argparse.ArgumentParser = get_parser()
     args: argparse.Namespace = parser.parse_args(argv)
-    args: argparse.Namespace = replace_args(args)
     logger.debug(args)
-    logger.info("Retrieve relevant files...")
-    relevant_files: List[str] = get_relevant_files(args)
 
-    if relevant_files is None or relevant_files == []:
+    logger.info("Setup...")
+    args_dict: Optional[dict] = setup_args(args)
+    if args_dict is None:
+        return ReturnCode.SETUP_FAILED
+
+    logger.info("Retrieve relevant files...")
+    relevant_files: List[str] = get_relevant_files(args_dict)
+    if relevant_files is None:
         return ReturnCode.RETRIEVAL_FAILED
 
     for i, relevant_file in enumerate(relevant_files):
@@ -46,21 +53,37 @@ def main(argv: Optional[List[str]]) -> int:
 
 def strip_argv(argv: Optional[List[str]]) -> List[str]:
     if argv is None:
-        logger.warning("Failed to load arguments, set to default.")
+        logger.error("Failed to load arguments, set to default.")
         return ["--help"]
     if len(argv) <= 1:
-        logger.info("Insufficient number of arguments, set to default.")
+        logger.error("Insufficient number of arguments, set to default.")
         return ["--help"]
     argv_copy: Optional[List[str]] = copy.deepcopy(argv)
     argv_copy.pop(0)  # remove first value (script path) of argv (sys.argv output)
     return argv_copy
 
 
-def replace_args(args: argparse.Namespace) -> argparse.Namespace:
-    args_copy: argparse.Namespace = copy.deepcopy(args)
-    if vars(args_copy)["paths"] == ["."]:
-        vars(args_copy)["paths"] = [os.getcwd()]
-    return args_copy
+def setup_args(args: argparse.Namespace) -> Optional[dict]:
+    args_dict: dict = vars(args)
+    # check assumptions
+    if args_dict is None:
+        return None
+    if not all(k in args_dict for k in ("paths", "matcher", "types", "limit")):
+        return None
+    # replace "." path input with current working directory
+    if args_dict["paths"] == ["."]:
+        args_dict["paths"] = [os.getcwd()]
+    directory_paths = []
+    # check if directories exist
+    for directory_path in args_dict["paths"]:
+        if not os.path.isdir(directory_path):
+            logger.error(f"Directory {directory_path} does not exist.")
+        else:
+            directory_paths.append(directory_path)
+    if not directory_paths:
+        logger.critical(f"No existing directory parsed.")
+        return None
+    return args_dict
 
 
 def get_parser() -> argparse.ArgumentParser:
@@ -76,11 +99,26 @@ def get_parser() -> argparse.ArgumentParser:
         help="Set directory paths.",
     )
     parser.add_argument(
+        "-m",
+        "--matcher",
+        default="eq",
+        type=str,
+        choices=["eq", "re"],
+        help="Set matcher technique.",
+    )
+    parser.add_argument(
         "-t",
         "--types",
         nargs="+",
         default=[".pdf"],
         choices=[".txt", ".pdf"],
         help="Set file types.",
+    )
+    parser.add_argument(
+        "-l",
+        "--limit",
+        default="10",
+        type=int,
+        help="Set limit for output.",
     )
     return parser
--- pycrawl/solver.py
+++ pycrawl/solver.py
@@ -1,7 +1,25 @@
-import argparse
+import os
 
-from typing import List
+from typing import List, Tuple, Optional
 
 
-def get_relevant_files(args: argparse.Namespace, limit: int = 10) -> List[str]:
-    pass
+def get_relevant_files(args: dict) -> Optional[List[str]]:
+    directory_paths: List[str] = args["paths"]
+    file_types: Tuple[str] = tuple(args["types"])
+    limit: int = args["limit"]
+    matcher: str = args["matcher"]
+
+    for directory_path in directory_paths:
+        for file in os.scandir(directory_path):
+            file_path: str = file.path
+            if not file_path.endswith(file_types):
+                continue
+            # get text
+            if file_path.endswith(".pdf"):
+                pass
+            elif file_path.endswith(".txt"):
+                pass
+            # get matches + map to file
+    # sort matches
+    # return matches, given limit
+    return None
